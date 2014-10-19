303bff370cea3fa9666a2d0a0e8e4814f69d9c8a
maximfedorov <f.maxym@gmail.com>
2014-10-19 23:23:19 +0300

    DelegateCommand

---
+++ Geo/Common/DelegateCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Geo.Common
+{
+	public class DelegateCommand : ICommand
+	{
+		private readonly Action<object> _executeAction;
+		private readonly Func<object, bool> _canExecuteFunc;
+
+		public event EventHandler CanExecuteChanged;
+
+		public DelegateCommand(Action<object> executeAction, Func<object, bool> canExcecuteFunc = null)
+		{
+			if (executeAction == null)
+				throw new ArgumentNullException(nameof(executeAction));
+
+			_executeAction = executeAction;
+			_canExecuteFunc = canExcecuteFunc;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			if (_canExecuteFunc == null)
+				return true;
+
+			return _canExecuteFunc(parameter);
+		}
+
+		public void Execute(object parameter)
+		{
+			_executeAction(parameter);
+		}
+
+		public void RaiseExecuteChanged()
+		{
+			if (CanExecuteChanged != null)
+				CanExecuteChanged(this, new EventArgs());
+		}
+	}
+}
